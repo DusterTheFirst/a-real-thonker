5ee458f4432e13df1cc20f5c843236e2d624a47f
DusterTheFirst <dusterthefirst@users.noreply.github.com>
2017-11-07 16:43:58 -0500

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,20 @@
+chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, {givememyinfobackplease: true}, (data) => {
+        console.log(data);
+        document.getElementById('cornerthonk').checked = data.cornerthinktoggled;
+        document.getElementById('bigboi').checked = data.bigboitoggled;
+    });        
+});
+
+document.getElementById('cornerthonk')
+.addEventListener('change', (e) => {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, {cornerthonk: document.getElementById('cornerthonk').checked});        
+    });
+});
+document.getElementById('bigboi')
+.addEventListener('change', (e) => {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, {bigboi: document.getElementById('bigboi').checked});        
+    });
+});
---
+++ thonk.js
@@ -0,0 +1,80 @@
+//cold
+console.log('%c🤔🤔🤔🤔', 'font-size:100px');
+console.log('%ci see you know how to open dev tools, good job', 'color: #bada55; font-size:30px');
+
+let topleft = document.createElement('div');
+topleft.innerHTML = "🤔";
+topleft.classList.add('thonk');
+topleft.classList.add('topleft');
+document.body.appendChild(topleft);
+
+let topright = document.createElement('div');
+topright.innerHTML = "🤔";
+topright.classList.add('thonk');
+topright.classList.add('topright');
+document.body.appendChild(topright);
+
+let bottomleft = document.createElement('div');
+bottomleft.innerHTML = "🤔";
+bottomleft.classList.add('thonk');
+bottomleft.classList.add('bottomleft');
+document.body.appendChild(bottomleft);
+
+let bottomright = document.createElement('div');
+bottomright.innerHTML = "🤔";
+bottomright.classList.add('thonk');
+bottomright.classList.add('bottomright');
+document.body.appendChild(bottomright);
+
+let bigboi = document.createElement('div');
+bigboi.innerHTML = "🤔";
+bigboi.classList.add('thonk');
+bigboi.classList.add('bigboi');
+document.body.appendChild(bigboi);
+
+//LIT 🔥🔥🔥👌
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log(request);
+    
+    if (request.cornerthonk !== undefined)
+        cornerthonktoggle(request.cornerthonk);
+    
+    if (request.bigboi !== undefined)
+        bigboitoggle(request.bigboi);
+
+    if (request.givememyinfobackplease) {
+        sendResponse({
+            cornerthinktoggled: cornerthinktoggled,
+            bigboitoggled: bigboitoggled
+        })
+    }
+});
+
+var cornerthinktoggled;
+function cornerthonktoggle(enabled) {
+    cornerthinktoggled = enabled;
+    if (enabled) {
+        topleft.classList.add('show');
+        topright.classList.add('show');
+        bottomleft.classList.add('show');
+        bottomright.classList.add('show');      
+    } else {
+        topleft.classList.remove('show');
+        topright.classList.remove('show');
+        bottomleft.classList.remove('show');
+        bottomright.classList.remove('show');  
+    }
+}
+
+var bigboitoggled;
+function bigboitoggle(enabled) {
+    bigboitoggled = enabled;
+    if (enabled) {
+        bigboi.classList.add('show');   
+    } else {
+        bigboi.classList.remove('show');
+    }
+}
+
+//just right ✋😩👌
